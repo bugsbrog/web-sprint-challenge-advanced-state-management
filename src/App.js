cc8fc678d8e093b48878eb540eb147965ab00ace
bugsbrog <bugsbrog@gmail.com>
2021-12-09 20:25:18 -0700

    Fixed something on the reducer file and worked on App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { fetchSmurfs } from './actions';
 
 import AddForm from './components/AddForm';
 import SmurfList from './components/SmurfList';
@@ -7,20 +9,26 @@ import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
-const App = ()=> {
-  return (
-    <div className="App">
-      <Header />
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchSmurfs();
+    }
 
-      <main>
-        <SmurfList/>
-        <AddForm/>
-      </main>
-    </div>
-  );
+    render() {
+        return (
+            <div className="App">
+                <Header />
+
+                 <main>
+                    <SmurfList/>
+                    <AddForm/>
+                 </main>
+                </div>
+        );
+    }
 }
 
-export default App;
+export default connect(null, { fetchSmurfs })(App);
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
